f874bb46d81fa488a55f4a9adeb82335fec0a976
Andrés Londoño <andreslon@outlook.com>
2017-05-22 00:11:40 -0500

    Implementacion de controlador para subir imagenes adjuntas

---
+++ src/BeautifulPlaces.Api/BeautifulPlaces.Api/Controllers/UploadController.cs
@@ -0,0 +1,59 @@
+﻿using BeautifulPlaces.Api.Utilities;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+
+namespace BeautifulPlaces.Api.Controllers
+{
+    public class UploadController : ApiController
+    {
+        private const string Container = "images";
+        public async Task<IHttpActionResult> Post()
+        {
+
+
+            if (!Request.Content.IsMimeMultipartContent("form-data"))
+            {
+                throw new HttpResponseException(HttpStatusCode.UnsupportedMediaType);
+            }
+
+            var accountName = ConfigurationManager.AppSettings["storage:account:name"];
+            var accountKey = ConfigurationManager.AppSettings["storage:account:key"];
+            var storageAccount = new CloudStorageAccount(new StorageCredentials(accountName, accountKey), true);
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+            CloudBlobContainer imagesContainer = blobClient.GetContainerReference(Container);
+            var provider = new AzureStorageMultipartFormDataStreamProvider(imagesContainer);
+
+            try
+            {
+                await Request.Content.ReadAsMultipartAsync(provider);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"An error has occured. Details: {ex.Message}");
+            }
+
+            // Retrieve the filename of the file you have uploaded
+            var filename = provider.FileData.FirstOrDefault()?.LocalFileName;
+            if (string.IsNullOrEmpty(filename))
+            {
+                return BadRequest("An error has occured while uploading your file. Please try again.");
+            }
+
+            return Ok($"https://andreslonstorage.blob.core.windows.net/images/{filename}");
+
+        }
+    }
+}
